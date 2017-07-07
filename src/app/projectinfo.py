aa2c9f6c8d677baa9b88b98543d6d7e098272ea0
Дмитрий Шлома <dmitry.shloma@oniip.local>
2017-07-07 06:48:59 +0000

    1. Удален неиспользуемый файл 2. Дублирующий код вынесен в файл pythonservice.pri 3. Добавлен поясняющий комментарий насчет ошибки

--- src/app/projectinfo.py
+++ src/app/projectinfo.py
@@ -21,3 +21,37 @@ def parse_project_version(version, param = ""):
         return version.split("_")[3]
 
     return version
+
+#def get_app_version(version_filename):
+    #file = open(version_filename, "r")
+    #lines = file.readlines()
+
+    #items = {}
+    #for line in lines:
+        #key_value = line.split("=")
+        #items[key_value[0].strip()] = key_value[1].strip()
+    #return items["project_version"]
+
+#def parse_app_version(version, param = ""):
+    #temp = version.split("_")[1]
+
+    #if param == "app_name":
+        #return version.split("_")[0]
+    #elif param == "build_year":
+        #return temp.split("~")[0].split(".")[0]
+    #elif param == "build_month":
+        #return temp.split("~")[0].split(".")[1]
+    #elif param == "build_day":
+        #return temp.split("~")[0].split(".")[2]
+    #elif param == "build_number":
+        #return temp.split("~")[0].split(".")[3]
+    #elif param == "desc":
+        #return temp.split("~")[1]
+    #elif param == "arch":
+        #return version.split("_")[2]
+    #elif param == "os":
+        #return version.split("_")[3]
+    #return version
+
+#def parse_plugin_version(version, param = ""):
+    #return version
--- src/app/sysinfo.pri
+++ src/app/sysinfo.pri
@@ -1,11 +1,4 @@
-## /// проверка того, что установлен Python версии 2.7
-version = 2.7
-unix: command = python$${version} --version 1>/dev/null 2>/dev/null; echo "$?"
-win32: command = python --version 1>nul 2>nul && echo 0 # FIXME: not working!
-return_code = $${system($${command})}
-!contains(return_code, 0) { # если return_code не равен 0
-    error(python $${version} is not installed)
-}
+include(pythonservice.pri)
 
 # @brief createDefinesSpecific_osVersion создание специфического определения
 # для getOsVersion()
